c41dcd5669167a6cab4f432894bdad96a6b1c511
Winston Fassett <winston.fassett@gmail.com>
2025-07-24 03:39:34 -0500

    Updated LibraryScreen.tsx

--- src/components/screens/LibraryScreen.tsx
+++ src/components/screens/LibraryScreen.tsx
@@ -136,7 +136,6 @@ export const LibraryScreen: React.FC<LibraryScreenProps> = ({
       </header>
     </div>
 
-        {/* Search Bar */}
         <div className="relative">
           <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
           <input
