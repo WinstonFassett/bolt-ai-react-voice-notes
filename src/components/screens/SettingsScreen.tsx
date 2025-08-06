84b398ef169147c972ab451b4c36db0b58172fb3
Winston Fassett <winston.fassett@gmail.com>
2025-08-06 17:49:44 -0500

    p

--- src/components/screens/SettingsScreen.tsx
+++ src/components/screens/SettingsScreen.tsx
@@ -6,7 +6,8 @@ import {
   CpuChipIcon as RobotIcon,
   TrashIcon,
   SunIcon,
-  MoonIcon
+  MoonIcon,
+  ComputerDesktopIcon
 } from '@heroicons/react/24/outline';
 
 // Import components
@@ -104,11 +105,7 @@ export const SettingsScreen: React.FC = () => {
                     theme === 'system' ? 'bg-primary text-primary-foreground' : 'bg-muted hover:bg-muted/80',
                   )}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-5 h-5">
-                    <rect x="2" y="3" width="20" height="14" rx="2" />
-                    <line x1="8" x2="16" y1="21" y2="21" />
-                    <line x1="12" x2="12" y1="17" y2="21" />
-                  </svg>
+                  <ComputerDesktopIcon className="w-5 h-5" />
                   System
                 </button>
                 <button
