221589796c0321e0471c5b014d404e1410392fa2
Winston Fassett <winston.fassett@gmail.com>
2025-08-07 09:32:13 -0500

    fix unused

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { initializeDebugTools } from './utils/debugInitializer';
+
+initializeDebugTools();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
